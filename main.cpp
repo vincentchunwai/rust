f0ac50fbf1d23e884732b0bf6cdff8e52df51d22
vincentchunwai <cheungchunwaivincent@gmail.com>
2024-04-21 19:12:07 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,52 +1,37 @@
 #include <GLFW/glfw3.h>
-#include <iostream>
-int main() {
-    // Initialize GLFW
-    if (!glfwInit()) {
-        const char* description;
-        glfwGetError(&description);
-        std::cerr << "GLFW initialization failed: " << description << std::endl;
-        return -1;
-    }
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
-        const char* description;
-        glfwGetError(&description);
-        std::cerr << "Failed to create GLFW window: " << description << std::endl;
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
 
-
-    // Make the window's context current
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    
-    if (!glfwGetCurrentContext()) {
-        std::cerr << "Failed to create OpenGL context" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
 
-
-    // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window)) {
-        std::cout << "Inside event loop" << std::endl;
-        // Set clear color (black)
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-        // Clear the color buffer
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap front and back buffers
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        // Poll for and process events
+        /* Poll for and process events */
         glfwPollEvents();
     }
-    glfwShowWindow(window);
-    // Cleanup
+
     glfwTerminate();
     return 0;
 }
